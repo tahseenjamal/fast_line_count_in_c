f5031d8968184ca7b84cf21fb95664a04cf8d783
Tahseen Jamal <tahseen.jamal@gmail.com>
2020-04-26 11:32:35 +0530

    code comments and minor optimization

--- main.c
+++ main.c
@@ -5,20 +5,23 @@
 #include <pthread.h>
 #include <stdatomic.h>
 
-#define THREADS 10
+// Declare number of threads to create
+#define THREADS 2
 
+// Structure that is create shared variables
 typedef struct Threadparameter {
 
     int *optimalbuffersize;
 
     FILE *file;
 
+    // atomic long pointer for thread safety
     atomic_long *counter;
 
 } Threadparameter;
 
 
-
+// Function spwaned as thread
 void *readbuffer(void *parameters) {
 
     Threadparameter *params = (Threadparameter*) parameters;
@@ -27,28 +30,30 @@ void *readbuffer(void *parameters) {
 
     int *optimalbuffersize = params->optimalbuffersize;
 
-    atomic_long *count = params->counter;
+    atomic_long *counter = params->counter;
 
     char *buffer = (char*) malloc(*optimalbuffersize * sizeof(char));
 
     int nread;
+
+    long local_thread_counter = 0;
 
     while(nread = fread(buffer, sizeof(char), *optimalbuffersize, file))
     {
 
         nread++;
 
-        while(nread--) {
+        while(nread--) 
 
             if(buffer[nread] == '\n')
-            {
-                atomic_fetch_add(count, 1);
-                
-            }
 
-        }
+                local_thread_counter++; 
+
 
     }
+
+    // After counting using the local counter, outside the loop, increment the atomic counter
+    atomic_fetch_add(counter, local_thread_counter);
 
     free(buffer);
 
@@ -73,14 +78,15 @@ int main(int argc, char const *argv[]) {
 
     int optimalbuffersize;
 
-    optimalbuffersize = 1024 * 1024;
+    // Predefine optimal disk block read size
+    optimalbuffersize = 1024 * 4;
 
+
+    // If optimal found using fstat
     if(fstatfs(fd, &fsinfo) != -1)
-    {
 
-        optimalbuffersize = fsinfo.f_bsize;
+        optimalbuffersize = fsinfo.f_bsize; // then overwrite the default one with the system optimal one
 
-    }
 
     if(!file) {
 
@@ -89,27 +95,28 @@ int main(int argc, char const *argv[]) {
         return -1;
     }
 
-    atomic_long count = 0;
+    atomic_long counter = 0; // Atomic counter initialized and this would be used by all threads
 
-    Threadparameter *params = (Threadparameter*) malloc(sizeof(Threadparameter));
+    Threadparameter *params = (Threadparameter*) malloc(sizeof(Threadparameter)); // Variable initialized using Threadparameter Struct
 
     params->file = file;
 
-    params->counter = &count;
+    params->counter = &counter;
 
     params->optimalbuffersize = &optimalbuffersize;
 
-    pthread_t *threads = (pthread_t*)malloc(sizeof(pthread_t) * THREADS);
+    pthread_t *threads = (pthread_t*)malloc(sizeof(pthread_t) * THREADS); // Number of threads initialized
 
     for(unsigned char loop = 0; loop < THREADS; loop++)
-    {
-        pthread_create(&threads[loop], NULL, readbuffer, (void*) params);
 
-        pthread_join(threads[loop], NULL);
-    }
+        pthread_create(&threads[loop], NULL, readbuffer, (void*) params); // Creating threads
+
+    for(unsigned char loop = 0; loop < THREADS; loop++)
+
+        pthread_join(threads[loop], NULL); // Joining threads to avoid program exiting before the threads complete
 
 
-    printf("%ld\n",count);
+    printf("%ld\n",counter); // Printing the total count of lines
 
     fclose(file);
 
